544380640faa09352d4ce8eef861ab52356754ca
celinesc <celinesc@umich.edu>
2019-10-17 15:33:09 -0400

    Continued rewriting code to fit with RTOS calls

--- color_sensor/color_sensor.ino
+++ color_sensor/color_sensor.ino
@@ -26,7 +26,7 @@ typedef struct
 
 typedef struct
 {
-  String Name;
+  double ppm;
   Pixel p;
 }Color;
 
@@ -61,94 +61,93 @@ void addColors()
 {
   int index = 0;
   Color c;
-  c.Name = "0.0";
+  c.ppm = 0.0;
   c.p = {0, 0, 0};
   Ammonia[index] = c;
 
-  c.Name = "0.0";
+  c.ppm = 0.0;
   c.p = {0, 0, 0};
   Nitrite[index] = c;
 
-  c.Name = "0.0";
+  c.ppm = 0.0;
   c.p = {0, 0, 0};
   Nitrate[index] = c;
 
   ++index;
 
-  c.Name = "0.25";
+  c.ppm = 0.25;
   c.p = {0, 0, 0};
   Ammonia[index] = c;
 
-  c.Name = "0.15";
+  c.ppm = 0.15;
   c.p = {0, 0, 0};
   Nitrite[index] = c;
 
-  c.Name = "0.5";
+  c.ppm = 0.5;
   c.p = {0, 0, 0};
   Nitrate[index] = c;
 
   ++index;
 
-  c.Name = "0.5";
+  c.ppm = 0.5;
   c.p = {0, 0, 0};
   Ammonia[index] = c;
 
-  c.Name = "0.3";
+  c.ppm = 0.3;
   c.p = {0, 0, 0};
   Nitrite[index] = c;
 
-  c.Name = "2.0";
+  c.ppm = 2.0;
   c.p = {0, 0, 0};
   Nitrate[index] = c;
 
   ++index;
 
-  c.Name = "3.0";
+  c.ppm = 3.0;
   c.p = {0, 0, 0};
   Ammonia[index] = c;
 
-  c.Name = "1.0";
+  c.ppm = 1.0;
   c.p = {0, 0, 0};
   Nitrite[index] = c;
 
-  c.Name = "5.0";
+  c.ppm = 5.0;
   c.p = {0, 0, 0};
   Nitrate[index] = c;
 
   ++index;
 
-  c.Name = "6.0";
+  c.ppm = 6.0;
   c.p = {0, 0, 0};
   Ammonia[index] = c;
 
-  c.Name = "1.5";
+  c.ppm = 1.5;
   c.p = {0, 0, 0};
   Nitrite[index] = c;
 
-  c.Name = "10.0";
+  c.ppm = 10.0;
   c.p = {0, 0, 0};
   Nitrate[index] = c;
 
   ++index;
 
-  c.Name = "3.0";
+  c.ppm = 3.0;
   c.p = {0, 0, 0};
   Nitrite[index] = c;
 
-  c.Name = "20.0";
+  c.ppm = 20.0;
   c.p = {0, 0, 0};
   Nitrate[index] = c;
 
   ++index;
 
-  c.Name = "10.0";
+  c.ppm = 10.0;
   c.p = {0, 0, 0};
   Nitrite[index] = c;
 
-  c.Name = "50.0";
+  c.ppm = 50.0;
   c.p = {0, 0, 0};
   Nitrate[index] = c;
-  
 }
 
 char MainMenu()
@@ -178,53 +177,53 @@ double FindMatch(colorData *rgb, ReadType type)
   {
     case AMMONIA:
     {
+      // start at highest ammonia level - false positive better than false negative
       for(int i = MAX_AMMONIA_COLORS - 1; i >= 0; --i)
       {
         if(SameColor(rgb, Ammonia[i]))
-          return Ammonia[i].Name.toDouble();
+          return Ammonia[i].ppm;
       }
       break;
     }
     case NITRITE:
     {
-
+      // start at highest nitrite level - false positive better than false negative
+      for(int i = MAX_NITRITE_NITRATE_COLORS - 1; i >= 0; --i)
+      {
+        if(SameColor(rgb, Nitrite[i]))
+          return Nitrite[i].ppm;
+      }
       break;
     }
     case NITRATE:
     {
-
+      // start at highest nitrate level - false positive better than false negative
+      for(int i = MAX_NITRITE_NITRATE_COLORS - 1; i >= 0; --i)
+      {
+        if(SameColor(rgb, Nitrate[i]))
+          return Nitrate[i].ppm;
+      }
       break;
     }   
   }
-
-
-  
-  uint8_t Idx=0;
-  bool Found=false;
-  while((Idx<NumColours)&(Found==false))
-  {
-    if((CheckColour(rgb->value[TCS230_RGB_R],Colours[Idx].Red)) &(CheckColour(rgb->value[TCS230_RGB_G],Colours[Idx].Green))&(CheckColour(rgb->value[TCS230_RGB_B],Colours[Idx].Blue)))
-          Found=true;
-    Idx++;
-  }
-  if(Found) return Idx-1; else return -1;
 }
 
-bool SameColor(colorData *rgb, ReadType type)
+bool SameColor(colorData *rgb, Color c)
 {
-  bool same = true;
-}
+  // values of stored color being compared against
+  uint8_t redExpected = rgb->value[TCS230_RGB_R];
+  uint8_t greenExpected = rgb->value[TCS230_RGB_R];
+  uint8_t blueExpected = rgb->value[TCS230_RGB_R];
 
-bool CheckColour(uint8_t ScanCol, uint8_t StoredCol)
-{
-  // returns true if matched on this single colour else false
-  int16_t StoreColLow,StoreColHigh;
-  StoreColLow=StoredCol-TOLERANCE;
-  StoreColHigh=StoredCol+TOLERANCE;
-  if(StoreColLow<0) StoreColLow=0;
-  if(StoreColHigh>255) StoreColHigh=255;
-  return ((ScanCol>=StoreColLow)&(ScanCol<=StoreColHigh));
-
+  // check if RGB values are within range specified by tolerance
+  if (c.p.R < (redExpected - TOLERANCE) or c.p.R > (redExpected + TOLERANCE))
+    return false;
+  if (c.p.G < (greenExpected - TOLERANCE) or c.p.G > (greenExpected + TOLERANCE))
+    return false;
+  if (c.p.B < (blueExpected - TOLERANCE) or c.p.B > (blueExpected + TOLERANCE))
+    return false;
+  Serial.print(F("\nFound Matching Color"));
+  return true;
 }
 
 char getChar()
