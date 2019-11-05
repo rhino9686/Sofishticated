d92739ccdecd2e47c0f4db50cbc168d2e8aa4903
Robert Cecil <robcecil@umich.edu>
2019-11-04 22:36:35 -0500

    fixed data server specifics

--- flask_server/tank.py
+++ flask_server/tank.py
@@ -1,22 +1,22 @@
 
 class Tank:
     
-    def __init__():
+    def __init__(self):
         self.currentTemp = 73
         self.currentpH = 6.5
         self.tempHistory = []
         self.pH_history = []
         
-    def getTemp():
+    def getTemp(self):
         return self.currentTemp
     
-    def getPH():
+    def getPH(self):
         return self.currentpH
     
-    def setTemp(temperature):
+    def setTemp(self,temperature):
         this.currentTemp = temperature
     
-    def setPH(pH_val):
+    def setPH(self,pH_val):
         this.currentpH = pH_val
         
 
